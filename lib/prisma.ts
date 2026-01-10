6ff6a2c6562e7fc767efba62eb9459818e4d9826
Beltann <146208568+Skeyiae@users.noreply.github.com>
2026-01-10 17:51:28 +0700

    Penyelesaian Bug Waktu Mau Deploy

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,10 @@
+// ...existing code...
 import "dotenv/config";
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from "@prisma/client";
+import * as Prisma from "@prisma/client";
 
+const { PrismaClient } = Prisma;
+// ...existing code...
 const connectionString = `${process.env.DATABASE_URL}`
 
 const adapter = new PrismaPg({ connectionString })
