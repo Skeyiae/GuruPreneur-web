38066ddef5407cb0ffd96f8c61be5ed34c931b3e
patpaw111 <mfaqih3562@gmail.com>
2026-01-11 21:50:33 +0700

    fix: remove prisma.config.ts and update env vars

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from "@prisma/client";
 // ...existing code...
-const connectionString = `${process.env.DATABASE_URL}`
+const connectionString = process.env.POSTGRES_PRISMA_URL || process.env.DATABASE_URL
 
 const adapter = new PrismaPg({ connectionString })
 const prisma = new PrismaClient({ adapter })
