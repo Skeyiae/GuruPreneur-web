14eb62a9f45b251fcf2863721b9307b33d34ec82
patpaw111 <mfaqih3562@gmail.com>
2026-01-11 22:19:48 +0700

    chore: setup deployment config

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,15 +2,6 @@
 import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 // ...existing code...
-const url = process.env.guru_preneur_PRISMA_DATABASE_URL
-    ?? process.env.PRISMA_DATABASE_URL
-    ?? process.env.guru_preneur_POSTGRES_URL
-    ?? process.env.POSTGRES_PRISMA_URL
-    ?? process.env.DATABASE_URL
-    ?? "";
-
-const prisma = new PrismaClient({
-    datasourceUrl: url,
-});
+const prisma = new PrismaClient();
 
 export { prisma }
