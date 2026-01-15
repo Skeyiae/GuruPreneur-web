7d3047addad507c593b0540c9a2f76660415db4f
Beltann <146208568+Skeyiae@users.noreply.github.com>
2026-01-15 23:34:02 +0700

    Update api,courses

--- src/app/api/tutor/courses/[id]/route.ts
+++ src/app/api/tutor/courses/[id]/route.ts
@@ -1,8 +1,7 @@
 import { NextResponse, NextRequest } from "next/server";
 import { currentUser } from "@clerk/nextjs/server";
-import { prisma } from "@/../lib/prisma";
+import { prisma } from '@/../lib/prisma';
 
-export const dynamic = "force-dynamic";
 // PUT Handler
 export async function PUT(
   req: NextRequest,
