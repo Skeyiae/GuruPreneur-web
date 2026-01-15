5f497669a078bb5016a2053048abeb703648931e
Beltann <146208568+Skeyiae@users.noreply.github.com>
2026-01-15 23:42:53 +0700

    Update, app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,19 @@
 import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import { ClerkProvider } from "@clerk/nextjs";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "GuruPreneur",
@@ -17,7 +28,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="id">
-        <body className="antialiased flex flex-col min-h-screen">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
+        >
           <Navbar />
 
           {/* INI PENTING */}
